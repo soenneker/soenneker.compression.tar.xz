5ce44777287719bac95f017842ffd075445657ab
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:12 -0500

    xml commenting

--- src/Soenneker.Compression.Tar.XZ/Abstract/ITarXZUtil.cs
+++ src/Soenneker.Compression.Tar.XZ/Abstract/ITarXZUtil.cs
@@ -8,6 +8,15 @@ namespace Soenneker.Compression.Tar.XZ.Abstract;
 /// </summary>
 public interface ITarXZUtil
 {
+    /// <summary>
+    /// Executes the decompress and extract operation.
+    /// </summary>
+    /// <param name="filePath">The file path.</param>
+    /// <param name="destinationDir">The destination dir.</param>
+    /// <param name="decompressedFileDir">The decompressed file dir.</param>
+    /// <param name="deleteDecompressedFile">The delete decompressed file.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     ValueTask DecompressAndExtract(string filePath, string destinationDir, string? decompressedFileDir = null, bool deleteDecompressedFile = true,
         CancellationToken cancellationToken = default);
 }
